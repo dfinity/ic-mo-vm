6fa947ffccc5a3f89b5cd5450c9c2eb1806ed7ba
Matthew Hammer <matthew.hammer@gmail.com>
2022-10-06 05:48:58 -0600

    eval update/query, using eval_prog, not eval_open_block.

--- lib.rs
+++ lib.rs
@@ -8,15 +8,23 @@ thread_local! {
     static CORE: RefCell<Core> = RefCell::new(Core::empty());
 }
 
-#[ic_cdk_macros::update]
-fn eval(prog: String) -> String {
+fn eval_(prog: String) -> String {
     CORE.with(|core| {
-        let program = parse(&prog).expect("parse error");
+        let p = parse(&prog).expect("parse error");
 
         let result_val =
-            core.borrow_mut().eval_open_block(
-                vec![], program).expect("eval error");
+            core.borrow_mut().eval_prog(p).expect("eval error");
 
         format!("{:?}", result_val)
     })
+}
+
+#[ic_cdk_macros::update]
+fn eval(prog: String) -> String {
+    eval_(prog)
+}
+
+#[ic_cdk_macros::query]
+fn eval_q(prog: String) -> String {
+    eval_(prog)
 }
