a2a6cf1c241c1c32f2e106cbef90f92ae9e0df28
Matthew Hammer <matthew.hammer@gmail.com>
2022-10-06 06:23:54 -0600

    Reset. (#4)
    * minimal demo of 'eval function in a can'
    
    * eval update/query, using eval_prog, not eval_open_block.
    
    * reset.

--- lib.rs
+++ lib.rs
@@ -1,7 +1,4 @@
-use motoko::{
-    check::parse,
-    vm_types::Core,
-};
+use motoko::{check::parse, vm_types::Core};
 use std::cell::RefCell;
 
 thread_local! {
@@ -12,8 +9,7 @@ fn eval_(prog: String) -> String {
     CORE.with(|core| {
         let p = parse(&prog).expect("parse error");
 
-        let result_val =
-            core.borrow_mut().eval_prog(p).expect("eval error");
+        let result_val = core.borrow_mut().eval_prog(p).expect("eval error");
 
         format!("{:?}", result_val)
     })
@@ -27,4 +23,9 @@ fn eval(prog: String) -> String {
 #[ic_cdk_macros::query]
 fn eval_q(prog: String) -> String {
     eval_(prog)
+}
+
+#[ic_cdk_macros::update]
+fn reset() -> () {
+    CORE.with(|core| *core.borrow_mut() = Core::empty())
 }
